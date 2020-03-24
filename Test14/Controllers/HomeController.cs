0cb7e2a6f6e41bcfb501ba0dfc448c9c0692bac6
Антон Богушевич <bam2000@gmail.com>
2020-03-25 01:28:49 +0300

    commit1

--- Test14/Controllers/HomeController.cs
+++ Test14/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Test14.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "adwadwdad";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "wadawdawd1231231";
 
             return View();
         }
